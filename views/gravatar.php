1bca4db99007255a128f94890b414ab4871840c4
Dave Widmer <dave@davewidmer.net>
2011-12-07 11:20:45 -0500

    Removing the view since it is not bundled into the class.

--- views/gravatar.php
+++
@@ -1,5 +0,0 @@
-<img src="<?php echo $gravatar->url;
-?>" width="<?php echo $gravatar->width;
-?>" height="<?php echo $gravatar->height;
-?>" alt="<?php echo $gravatar->alt;
-?>" class="<?php echo $gravatar->class; ?>" />
