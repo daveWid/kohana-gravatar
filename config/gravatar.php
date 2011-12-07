77477a8466d74a78cd2cf0531fac7c976a4e8993
Dave Widmer <dave@davewidmer.net>
2011-12-07 10:04:00 -0500

    Updating the configuration file.

--- config/gravatar.php
+++ config/gravatar.php
@@ -1,13 +1,19 @@
 <?php defined('SYSPATH') or die('No direct script access.');
-// Gravatar config array
+
 return array
 (
-	'default' => array
-	(
-		'default'	=> 'wavatar',	// The default image, FALSE is the G, can also use identicon, monsterid, wavatar or 404
-		'size'		=> 60,			// Size of the image in pixels
-		'rating'	=> Gravatar::R,	// Default rating
-		'class'		=> 'gravatar',		// The css class to apply, FALSE for none
-		'alt'		=> FALSE,		// ALT tag for image, FALSE for empty string
+	// Size of the image in pixels
+	'size'    => 60,
+
+	// The default image, FALSE is the G, can also use identicon, monsterid, wavatar, retro or 404
+	'default' => 'wavatar',
+
+	// Default rating [Gravatar::G, Gravatar::PG, Gravatar::R, Gravatar::X]
+	'rating'  => Gravatar::R,
+
+	// Additional attributes that are added into the <img> tag
+	'attrs'   => array(
+		'class' => "gravatar",
+		'alt' => "user gravatar"
 	),
 );
